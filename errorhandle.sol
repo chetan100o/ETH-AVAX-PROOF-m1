d591b275fd176894c476534245434a9903f637b7
chetan100o <137300850+chetan100o@users.noreply.github.com>
2023-07-09 13:48:15 +0530

    Update errorhandle.sol

--- errorhandle.sol
+++ errorhandle.sol
@@ -7,15 +7,15 @@ contract Attend {
     mapping(address => bool) public hasAttended;
 
     function markAtendance() public {
-        require(!hasAttended[msg.sender], "Already marked"); // Check if the sender has already voted
+        require(!hasAttended[msg.sender], "Already marked"); // Check if the person has already marked attendance
 
         totalAttendance++;
         hasAttended[msg.sender] = true;
 
         if (totalAttendance > 100) {
-            revert("marking limit exceeded"); // Revert the transaction if the voting limit is exceeded
+            revert("marking limit exceeded"); // Revert the transaction if the attendend limit is exceeded
         }
 
-        assert(totalAttendance <= 100); // Ensure that the total votes does not exceed the limit
+        assert(totalAttendance <= 100); // Ensure that the total attended does not exceed the limit
     }
 }
